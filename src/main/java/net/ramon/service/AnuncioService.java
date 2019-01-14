32736f85218d5aecd5f9d6e6937ede3a7bcb6a01
Ramón Lozano <niksokupa@gmail.com>
2019-01-14 13:43:16 +0100

    probando y pensando cosas

--- src/main/java/net/ramon/service/AnuncioService.java
+++ src/main/java/net/ramon/service/AnuncioService.java
@@ -1,6 +1,11 @@
 package net.ramon.service;
 
+import java.sql.Connection;
 import javax.servlet.http.HttpServletRequest;
+import net.ramon.bean.ReplyBean;
+import net.ramon.connection.publicinterface.ConnectionInterface;
+import net.ramon.constant.ConnectionConstants;
+import net.ramon.factory.ConnectionFactory;
 import net.ramon.service.genericServiceImplementation.GenericServiceImplementation;
 import net.ramon.service.publicServiceInterface.ServiceInterface;
 
@@ -9,5 +14,29 @@ public class AnuncioService extends GenericServiceImplementation implements Serv
     public AnuncioService(HttpServletRequest oRequest) {
         super(oRequest);
         ob = oRequest.getParameter("ob");
+    }
+
+    @Override
+    public ReplyBean create() throws Exception {
+        ReplyBean oReplyBean;
+        ConnectionInterface oConnectionPool = ConnectionFactory.getConnection(ConnectionConstants.connectionPool);
+        Connection oConnection = oConnectionPool.newConnection();
+        try {
+            oConnection.setAutoCommit(false);
+            //Recojo los datos del anuncio
+            String strJsonAnuncioFromClient = oRequest.getParameter("anuncio");
+            //Recojo los nombres de la foto
+            String strJsonExtrasFromClient = oRequest.getParameter("extras");
+            //Recojo las ID's de los extras
+            String strJsonFotosFromClient = oRequest.getParameter("fotos");
+            if(strJsonFotosFromClient != null){
+                
+            }
+        } catch (Exception ex) {
+            throw new Exception("ERROR: Service level: create method: " + ob + " object", ex);
+        } finally {
+            oConnectionPool.disposeConnection();
+        }
+        return oReplyBean;
     }
 }
--- src/main/java/net/ramon/service/FotosAnuncioService.java
+++ src/main/java/net/ramon/service/FotosAnuncioService.java
@@ -1,28 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package net.ramon.service;
-
-import javax.servlet.http.HttpServletRequest;
-import net.ramon.bean.ReplyBean;
-
-/**
- *
- * @author ramon
- */
-public class FotosAnuncioService {
-       HttpServletRequest oRequest;
-	String ob = null;
-
-	public FotosAnuncioService(HttpServletRequest oRequest) {
-		super();
-		this.oRequest = oRequest;
-		ob = oRequest.getParameter("ob");
-	}
-        
-        public ReplyBean create() throws Exception {
-
-    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package net.ramon.service;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import net.ramon.bean.ReplyBean;
+//
+///**
+// *
+// * @author ramon
+// */
+//public class FotosAnuncioService {
+//       HttpServletRequest oRequest;
+//	String ob = null;
+//
+//	public FotosAnuncioService(HttpServletRequest oRequest) {
+//		super();
+//		this.oRequest = oRequest;
+//		ob = oRequest.getParameter("ob");
+//	}
+//        
+//        public ReplyBean create() throws Exception {
+//
+//    }
+//}
