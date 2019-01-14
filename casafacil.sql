1c42bdcfdd0e953c82542aa1570e19bf1c7f17f3
a044525499y <a044525499y@s09pc05>
2019-01-14 19:34:46 +0100

    cambios bbdd

--- casafacil.sql
+++ casafacil.sql
@@ -1,445 +1,221 @@
--- phpMyAdmin SQL Dump
--- version 4.8.2
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost:3306
--- Tiempo de generación: 11-01-2019 a las 10:54:36
--- Versión del servidor: 5.7.23
--- Versión de PHP: 7.1.21
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `casafacil`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `anuncio`
---
-
-CREATE TABLE `anuncio` (
-  `id` int(11) NOT NULL,
-  `titulo` varchar(255) DEFAULT NULL,
-  `habitaciones` int(11) DEFAULT NULL,
-  `banyos` int(11) DEFAULT NULL,
-  `metroscasa` int(11) DEFAULT NULL,
-  `metrosterreno` int(11) DEFAULT NULL,
-  `direccion` varchar(255) DEFAULT NULL,
-  `fechacreacion` date DEFAULT NULL,
-  `fechaupdate` date DEFAULT NULL,
-  `precio` int(11) DEFAULT NULL,
-  `descripcion` mediumtext,
-  `id_usuario` int(11) NOT NULL,
-  `id_tipoinmueble` int(11) NOT NULL,
-  `id_tipovia` int(11) NOT NULL,
-  `id_barrio` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `barrio`
---
-
-CREATE TABLE `barrio` (
-  `id` int(11) NOT NULL,
-  `desc` varchar(255) DEFAULT NULL,
-  `codpostal` int(11) DEFAULT NULL,
-  `id_ciudad` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `barrio`
---
-
-INSERT INTO `barrio` (`id`, `desc`, `codpostal`, `id_ciudad`) VALUES
-(1, 'Cabanyal', 46011, 2);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `ciudad`
---
-
-CREATE TABLE `ciudad` (
-  `id` int(11) NOT NULL,
-  `desc` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `ciudad`
---
-
-INSERT INTO `ciudad` (`id`, `desc`) VALUES
-(1, 'Castellón'),
-(2, 'Valencia'),
-(3, 'Alicante');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `extras`
---
-
-CREATE TABLE `extras` (
-  `id` int(11) NOT NULL,
-  `desc` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `extras`
---
-
-INSERT INTO `extras` (`id`, `desc`) VALUES
-(1, 'Piscina'),
-(2, 'Garaje Privado'),
-(3, 'Garaje Compartido'),
-(4, 'Calefacción');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `extras_anuncio`
---
-
-CREATE TABLE `extras_anuncio` (
-  `id_anuncio` int(11) NOT NULL,
-  `id_extras` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `favoritos`
---
-
-CREATE TABLE `favoritos` (
-  `id_usuario` int(11) NOT NULL,
-  `id_anuncio` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `fotos`
---
-
-CREATE TABLE `fotos` (
-  `id` int(11) NOT NULL,
-  `ruta` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `fotos`
---
-
-INSERT INTO `fotos` (`id`, `ruta`) VALUES
-(1, 'asd1.png'),
-(2, 'asd2.png'),
-(3, 'asd3.png'),
-(4, 'asd4.png');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `fotos_anuncio`
---
-
-CREATE TABLE `fotos_anuncio` (
-  `id_anuncio` int(11) NOT NULL,
-  `id_fotos` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipoinmueble`
---
-
-CREATE TABLE `tipoinmueble` (
-  `id` int(11) NOT NULL,
-  `desc` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `tipoinmueble`
---
-
-INSERT INTO `tipoinmueble` (`id`, `desc`) VALUES
-(1, 'Piso'),
-(2, 'Casa'),
-(3, 'Chalet'),
-(4, 'Ático');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipousuario`
---
-
-CREATE TABLE `tipousuario` (
-  `id` int(11) NOT NULL,
-  `desc` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `tipousuario`
---
-
-INSERT INTO `tipousuario` (`id`, `desc`) VALUES
-(1, 'Administrador'),
-(2, 'Usuario');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipovia`
---
-
-CREATE TABLE `tipovia` (
-  `id` int(11) NOT NULL,
-  `desc` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `tipovia`
---
-
-INSERT INTO `tipovia` (`id`, `desc`) VALUES
-(1, 'Calle'),
-(2, 'Avenida'),
-(3, 'Plaza'),
-(4, 'Camino');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuario`
---
-
-CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL,
-  `nickname` varchar(255) DEFAULT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `apellido1` varchar(255) DEFAULT NULL,
-  `apellido2` varchar(255) DEFAULT NULL,
-  `correo` varchar(255) DEFAULT NULL,
-  `telefono` int(11) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `id_tipousuario` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `usuario`
---
-
-INSERT INTO `usuario` (`id`, `nickname`, `nombre`, `apellido1`, `apellido2`, `correo`, `telefono`, `password`, `id_tipousuario`) VALUES
-(1, 'admin', 'nombre', 'apellido1', 'apellido2', 'correo@correo.es', 657465431, 'admin', 1);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `anuncio`
---
-ALTER TABLE `anuncio`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_anuncio_usuario1_idx` (`id_usuario`),
-  ADD KEY `fk_anuncio_tipoinmueble1_idx` (`id_tipoinmueble`),
-  ADD KEY `fk_anuncio_tipovia1_idx` (`id_tipovia`),
-  ADD KEY `fk_anuncio_barrio1_idx` (`id_barrio`);
-
---
--- Indices de la tabla `barrio`
---
-ALTER TABLE `barrio`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_barrio_ciudad1_idx` (`id_ciudad`);
-
---
--- Indices de la tabla `ciudad`
---
-ALTER TABLE `ciudad`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `extras`
---
-ALTER TABLE `extras`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `extras_anuncio`
---
-ALTER TABLE `extras_anuncio`
-  ADD PRIMARY KEY (`id_anuncio`,`id_extras`),
-  ADD KEY `fk_Anuncio_has_Extras_Extras1_idx` (`id_extras`),
-  ADD KEY `fk_Anuncio_has_Extras_Anuncio_idx` (`id_anuncio`);
-
---
--- Indices de la tabla `favoritos`
---
-ALTER TABLE `favoritos`
-  ADD PRIMARY KEY (`id_usuario`,`id_anuncio`),
-  ADD KEY `fk_usuario_has_anuncio_anuncio1_idx` (`id_anuncio`),
-  ADD KEY `fk_usuario_has_anuncio_usuario1_idx` (`id_usuario`);
-
---
--- Indices de la tabla `fotos`
---
-ALTER TABLE `fotos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `fotos_anuncio`
---
-ALTER TABLE `fotos_anuncio`
-  ADD PRIMARY KEY (`id_anuncio`,`id_fotos`),
-  ADD KEY `fk_anuncio_has_fotos_fotos1_idx` (`id_fotos`),
-  ADD KEY `fk_anuncio_has_fotos_anuncio1_idx` (`id_anuncio`);
-
---
--- Indices de la tabla `tipoinmueble`
---
-ALTER TABLE `tipoinmueble`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `tipousuario`
---
-ALTER TABLE `tipousuario`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `tipovia`
---
-ALTER TABLE `tipovia`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_usuario_tipousuario1_idx` (`id_tipousuario`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `anuncio`
---
-ALTER TABLE `anuncio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `barrio`
---
-ALTER TABLE `barrio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT de la tabla `ciudad`
---
-ALTER TABLE `ciudad`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `extras`
---
-ALTER TABLE `extras`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `fotos`
---
-ALTER TABLE `fotos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `tipoinmueble`
---
-ALTER TABLE `tipoinmueble`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `tipousuario`
---
-ALTER TABLE `tipousuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT de la tabla `tipovia`
---
-ALTER TABLE `tipovia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `anuncio`
---
-ALTER TABLE `anuncio`
-  ADD CONSTRAINT `fk_anuncio_barrio1` FOREIGN KEY (`id_barrio`) REFERENCES `barrio` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_anuncio_tipoinmueble1` FOREIGN KEY (`id_tipoinmueble`) REFERENCES `tipoinmueble` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_anuncio_tipovia1` FOREIGN KEY (`id_tipovia`) REFERENCES `tipovia` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_anuncio_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `barrio`
---
-ALTER TABLE `barrio`
-  ADD CONSTRAINT `fk_barrio_ciudad1` FOREIGN KEY (`id_ciudad`) REFERENCES `ciudad` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `extras_anuncio`
---
-ALTER TABLE `extras_anuncio`
-  ADD CONSTRAINT `fk_Anuncio_has_Extras_Anuncio` FOREIGN KEY (`id_anuncio`) REFERENCES `anuncio` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Anuncio_has_Extras_Extras1` FOREIGN KEY (`id_extras`) REFERENCES `extras` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `favoritos`
---
-ALTER TABLE `favoritos`
-  ADD CONSTRAINT `fk_usuario_has_anuncio_anuncio1` FOREIGN KEY (`id_anuncio`) REFERENCES `anuncio` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_usuario_has_anuncio_usuario1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `fotos_anuncio`
---
-ALTER TABLE `fotos_anuncio`
-  ADD CONSTRAINT `fk_anuncio_has_fotos_anuncio1` FOREIGN KEY (`id_anuncio`) REFERENCES `anuncio` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_anuncio_has_fotos_fotos1` FOREIGN KEY (`id_fotos`) REFERENCES `fotos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `fk_usuario_tipousuario1` FOREIGN KEY (`id_tipousuario`) REFERENCES `tipousuario` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- MySQL Script generated by MySQL Workbench
+-- lun 14 ene 2019 15:42:25 CET
+-- Model: New Model    Version: 1.0
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Schema casafacil
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema casafacil
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `casafacil` DEFAULT CHARACTER SET utf8 ;
+USE `casafacil` ;
+
+-- -----------------------------------------------------
+-- Table `casafacil`.`tipousuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `casafacil`.`tipousuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `desc` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `casafacil`.`usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `casafacil`.`usuario` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nickname` VARCHAR(255) NULL,
+  `nombre` VARCHAR(255) NULL,
+  `apellido1` VARCHAR(255) NULL,
+  `apellido2` VARCHAR(255) NULL,
+  `correo` VARCHAR(255) NULL,
+  `telefono` INT NULL,
+  `password` VARCHAR(255) NULL,
+  `id_tipousuario` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_usuario_tipousuario1_idx` (`id_tipousuario` ASC),
+  CONSTRAINT `fk_usuario_tipousuario1`
+    FOREIGN KEY (`id_tipousuario`)
+    REFERENCES `casafacil`.`tipousuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `casafacil`.`tipoinmueble`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `casafacil`.`tipoinmueble` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `desc` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `casafacil`.`tipovia`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `casafacil`.`tipovia` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `desc` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `casafacil`.`ciudad`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `casafacil`.`ciudad` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `desc` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `casafacil`.`barrio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `casafacil`.`barrio` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `desc` VARCHAR(255) NULL,
+  `codpostal` INT NULL,
+  `id_ciudad` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_barrio_ciudad1_idx` (`id_ciudad` ASC),
+  CONSTRAINT `fk_barrio_ciudad1`
+    FOREIGN KEY (`id_ciudad`)
+    REFERENCES `casafacil`.`ciudad` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `casafacil`.`anuncio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `casafacil`.`anuncio` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `titulo` VARCHAR(255) NULL,
+  `habitaciones` INT NULL,
+  `banyos` INT NULL,
+  `metroscasa` INT NULL,
+  `metrosterreno` INT NULL,
+  `direccion` VARCHAR(255) NULL,
+  `fechacreacion` DATE NULL,
+  `fechaupdate` DATE NULL,
+  `precio` INT NULL,
+  `descripcion` MEDIUMTEXT NULL,
+  `id_usuario` INT NOT NULL,
+  `id_tipoinmueble` INT NOT NULL,
+  `id_tipovia` INT NOT NULL,
+  `id_barrio` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_anuncio_usuario1_idx` (`id_usuario` ASC),
+  INDEX `fk_anuncio_tipoinmueble1_idx` (`id_tipoinmueble` ASC),
+  INDEX `fk_anuncio_tipovia1_idx` (`id_tipovia` ASC),
+  INDEX `fk_anuncio_barrio1_idx` (`id_barrio` ASC),
+  CONSTRAINT `fk_anuncio_usuario1`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `casafacil`.`usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_anuncio_tipoinmueble1`
+    FOREIGN KEY (`id_tipoinmueble`)
+    REFERENCES `casafacil`.`tipoinmueble` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_anuncio_tipovia1`
+    FOREIGN KEY (`id_tipovia`)
+    REFERENCES `casafacil`.`tipovia` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_anuncio_barrio1`
+    FOREIGN KEY (`id_barrio`)
+    REFERENCES `casafacil`.`barrio` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `casafacil`.`extras`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `casafacil`.`extras` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `desc` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `casafacil`.`extras_anuncio`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `casafacil`.`extras_anuncio` (
+  `id_anuncio` INT NOT NULL,
+  `id_extras` INT NOT NULL,
+  PRIMARY KEY (`id_anuncio`, `id_extras`),
+  INDEX `fk_Anuncio_has_Extras_Extras1_idx` (`id_extras` ASC),
+  INDEX `fk_Anuncio_has_Extras_Anuncio_idx` (`id_anuncio` ASC),
+  CONSTRAINT `fk_Anuncio_has_Extras_Anuncio`
+    FOREIGN KEY (`id_anuncio`)
+    REFERENCES `casafacil`.`anuncio` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Anuncio_has_Extras_Extras1`
+    FOREIGN KEY (`id_extras`)
+    REFERENCES `casafacil`.`extras` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `casafacil`.`favoritos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `casafacil`.`favoritos` (
+  `id_usuario` INT NOT NULL,
+  `id_anuncio` INT NOT NULL,
+  PRIMARY KEY (`id_usuario`, `id_anuncio`),
+  INDEX `fk_usuario_has_anuncio_anuncio1_idx` (`id_anuncio` ASC),
+  INDEX `fk_usuario_has_anuncio_usuario1_idx` (`id_usuario` ASC),
+  CONSTRAINT `fk_usuario_has_anuncio_usuario1`
+    FOREIGN KEY (`id_usuario`)
+    REFERENCES `casafacil`.`usuario` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_usuario_has_anuncio_anuncio1`
+    FOREIGN KEY (`id_anuncio`)
+    REFERENCES `casafacil`.`anuncio` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `casafacil`.`fotos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `casafacil`.`fotos` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `ruta` VARCHAR(255) NULL,
+  `id_anuncio` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_fotos_anuncio1_idx` (`id_anuncio` ASC),
+  CONSTRAINT `fk_fotos_anuncio1`
+    FOREIGN KEY (`id_anuncio`)
+    REFERENCES `casafacil`.`anuncio` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
