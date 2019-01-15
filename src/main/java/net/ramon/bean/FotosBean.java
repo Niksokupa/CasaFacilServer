07b6fe1efb0f20b99be7df5261413f6684011f6c
a044525499y <a044525499y@s09pc05>
2019-01-15 17:10:54 +0100

    vamos avanzando

--- src/main/java/net/ramon/bean/FotosBean.java
+++ src/main/java/net/ramon/bean/FotosBean.java
@@ -6,25 +6,53 @@ import java.sql.ResultSet;
 import com.google.gson.annotations.Expose;
 import net.ramon.bean.genericBeanInterface.GenericBeanImplementation;
 import net.ramon.bean.publicBeanInterface.BeanInterface;
+import net.ramon.dao.publicDaoInterface.DaoInterface;
+import net.ramon.factory.DaoFactory;
 import net.ramon.helper.EncodingHelper;
 
-public class FotosBean extends GenericBeanImplementation implements BeanInterface{
+public class FotosBean extends GenericBeanImplementation implements BeanInterface {
 
     @Expose
     private String ruta;
+    @Expose
+    private Integer id_anuncio;
+    @Expose(deserialize = false)
+    private AnuncioBean obj_Anuncio;
 
-    public String getDesc() {
+    public AnuncioBean getObj_Anuncio() {
+        return obj_Anuncio;
+    }
+
+    public void setObj_Anuncio(AnuncioBean obj_Anuncio) {
+        this.obj_Anuncio = obj_Anuncio;
+    }
+
+    public Integer getId_anuncio() {
+        return id_anuncio;
+    }
+
+    public void setId_anuncio(Integer id_anuncio) {
+        this.id_anuncio = id_anuncio;
+    }
+
+    public String getRuta() {
         return ruta;
     }
 
-    public void setDesc(String ruta) {
+    public void setRuta(String ruta) {
         this.ruta = ruta;
     }
 
     @Override
     public FotosBean fill(ResultSet oResultSet, Connection oConnection, Integer expand) throws Exception {
         this.setId(oResultSet.getInt("id"));
-        this.setDesc(oResultSet.getString("ruta"));
+        this.setRuta(oResultSet.getString("ruta"));
+        if (expand > 0) {
+            DaoInterface oAnuncioDao = DaoFactory.getDao(oConnection, "anuncio");
+            this.setObj_Anuncio((AnuncioBean) oAnuncioDao.get(oResultSet.getInt("id_anuncio"), expand));
+        } else {
+            this.setId_anuncio(oResultSet.getInt("id_anuncio"));
+        }
         return this;
     }
 
@@ -32,23 +60,34 @@ public class FotosBean extends GenericBeanImplementation implements BeanInterfac
     public String getColumns() {
         String strColumns = "";
         strColumns += "fotos.id,";
-        strColumns += "fotos.ruta";
+        strColumns += "fotos.ruta,";
+        strColumns += "fotos.id_anuncio";
         return strColumns;
     }
 
     @Override
     public String getValues() {
+        if (id_anuncio == 0) {
+            id_anuncio = obj_Anuncio.getId();
+        }
+        
         String strColumns = "";
         strColumns += "null,";
-        strColumns += EncodingHelper.quotate(ruta);
+        strColumns += EncodingHelper.quotate(ruta) + ",";
+        strColumns += id_anuncio;
         return strColumns;
     }
 
     @Override
     public String getPairs() {
+        if (id_anuncio == 0) {
+            id_anuncio = obj_Anuncio.getId();
+        }
+        
         String strPairs = "";
         strPairs += "fotos.id=" + id + ",";
-        strPairs += "fotos.desc=" + ruta;
+        strPairs += "fotos.desc=" + ruta + ",";
+        strPairs += "fotos.id_anuncio=" + id_anuncio;
         strPairs += " WHERE fotos.id=" + id;
         return strPairs;
     }
--- src/main/java/net/ramon/service/AnuncioService.java
+++ src/main/java/net/ramon/service/AnuncioService.java
@@ -5,11 +5,13 @@ import com.google.gson.GsonBuilder;
 import java.sql.Connection;
 import javax.servlet.http.HttpServletRequest;
 import net.ramon.bean.ExtrasAnuncioBean;
+import net.ramon.bean.FotosBean;
 import net.ramon.bean.ReplyBean;
 import net.ramon.bean.publicBeanInterface.BeanInterface;
 import net.ramon.connection.publicinterface.ConnectionInterface;
 import net.ramon.constant.ConnectionConstants;
 import net.ramon.dao.ExtrasAnuncioDao;
+import net.ramon.dao.FotosDao;
 import net.ramon.dao.publicDaoInterface.DaoInterface;
 import net.ramon.factory.BeanFactory;
 import net.ramon.factory.ConnectionFactory;
@@ -37,22 +39,19 @@ public class AnuncioService extends GenericServiceImplementation implements Serv
             String strJsonAnuncioFromClient = oRequest.getParameter("anuncio");
             //Recojo los ID de los extras
             String strJsonExtrasFromClient = oRequest.getParameter("extras");
-            strJsonExtrasFromClient = strJsonExtrasFromClient.replace("[", "");
-            strJsonExtrasFromClient = strJsonExtrasFromClient.replace("]", "");
-            //Aquí tengo un array con todos los extras elegidos para el anuncio.
-            String extrasArray[] = strJsonExtrasFromClient.split(delimiter);
-//            String strJsonFotosFromClient = oRequest.getParameter("fotos");
-//            strJsonFotosFromClient = strJsonFotosFromClient.replace("[", "");
-//            strJsonFotosFromClient = strJsonFotosFromClient.replace("\"", "");
-//            strJsonFotosFromClient = strJsonFotosFromClient.replace("]", "");
-//            //Aquí tengo un array con todos los nombres elegidos para cada foto.
-//            String fotosArray[] = strJsonFotosFromClient.split(delimiter);
+            //Recojo los nombres de las fotos
+            String strJsonFotosFromClient = oRequest.getParameter("fotos");
+
             BeanInterface oBeanAnuncio = BeanFactory.getBeanFromJson(ob, oGson, strJsonAnuncioFromClient);
             DaoInterface oDao = DaoFactory.getDao(oConnection, ob);
             oBeanAnuncio = oDao.create(oBeanAnuncio);
-            
+
             if (strJsonExtrasFromClient != null) {
-                for(int i = 0; i<extrasArray.length;i++){
+                strJsonExtrasFromClient = strJsonExtrasFromClient.replace("[", "");
+                strJsonExtrasFromClient = strJsonExtrasFromClient.replace("]", "");
+                //Aquí tengo un array con todos los extras elegidos para el anuncio.
+                String extrasArray[] = strJsonExtrasFromClient.split(delimiter);
+                for (int i = 0; i < extrasArray.length; i++) {
                     ExtrasAnuncioBean extrasAnuncioBean = new ExtrasAnuncioBean();
                     extrasAnuncioBean.setId_anuncio(oBeanAnuncio.getId());
                     extrasAnuncioBean.setId_extras(Integer.parseInt(extrasArray[i]));
@@ -60,6 +59,23 @@ public class AnuncioService extends GenericServiceImplementation implements Serv
                     extrasAnuncioBean = (ExtrasAnuncioBean) extrasAnuncioDao.create(extrasAnuncioBean);
                 }
             }
+            if (strJsonFotosFromClient != null) {
+                strJsonFotosFromClient = strJsonFotosFromClient.replace("[", "");
+                strJsonFotosFromClient = strJsonFotosFromClient.replace("\"", "");
+                strJsonFotosFromClient = strJsonFotosFromClient.replace("]", "");
+                //Aquí tengo un array con todos los nombres elegidos para cada foto.
+                String fotosArray[] = strJsonFotosFromClient.split(delimiter);
+                for (int i = 0; i < fotosArray.length; i++) {
+                    FotosBean fotosBean = new FotosBean();
+                    fotosBean.setRuta(fotosArray[i]);
+                    fotosBean.setId_anuncio(oBeanAnuncio.getId());
+                    FotosDao fotosDao = new FotosDao(oConnection, "fotos");
+                    fotosBean = (FotosBean) fotosDao.create(fotosBean);
+                    //Falta poner el addImage de FotosService cuando sepa como funciona desde Angular7
+                }
+            }
+
+            oConnection.commit();
             oReplyBean = new ReplyBean(200, oGson.toJson(oBeanAnuncio));
         } catch (Exception ex) {
             throw new Exception("ERROR: Service level: create method: " + ob + " object", ex);
